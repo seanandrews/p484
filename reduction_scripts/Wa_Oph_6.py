15b134bc4613ed6bc5ad4db045534ffe4567cd9d
j6626 <janehuang212@gmail.com>
2017-06-04 15:42:46 -0400

    updated Wa Oph 6 script

--- reduction_scripts/Wa_Oph_6.py
+++ reduction_scripts/Wa_Oph_6.py
@@ -5,9 +5,11 @@ Note that the imaging algorithms were rewritten significantly between CASA 4.5.3
 
 field = 'Wa_Oph_6'
 
-######
+##################################################################
+##################################################################
 ## short baseline data
-######
+##################################################################
+##################################################################
 
 SB1 = 'calibrated_final.ms' #replace as appropriate
 SB1refant = 'DA49'
@@ -208,7 +210,74 @@ clean(vis=SB1_contms_ap1,
 
 ### We are now done with self-cal of the continuum of SB1 and rename the final measurement set. 
 SB1_contms_final = field+'_'+tag+'_contfinal.ms'
-os.system('mv '+SB1_contms_ap1+' '+SB1_contms_final)
+os.system('cp -r '+SB1_contms_ap1+' '+SB1_contms_final)
+
+##############################
+# Reduction of CO data in SB1
+#############################
+
+#split out the CO 2-1 spectral window
+# Average the channels within spws
+linespw = '0'
+SB1_CO_ms = '/pool/firebolt1/LPscratch/Wa_Oph_6/'+field+'_'+tag+'_CO21.ms'
+os.system('rm -rf ' + SB1_CO_ms + '*')
+split2(vis=SB1,
+       field = field,
+       spw=linespw,      
+       outputvis=SB1_CO_ms, 
+       datacolumn='data')
+
+applycal(vis=SB1_CO_ms, spw='0', gaintable=[SB1_p1, SB1_p2, SB1_ap1], calwt=T, flagbackup=F)
+
+SB1_CO_mscontsub = SB1_CO_ms+'.contsub'
+os.system('rm -rf '+SB1_CO_mscontsub 
+fitspw = '0:0~1870;2000~3839' # channels for fitting continuum
+uvcontsub(vis=SB1_CO_ms,
+          spw=linespw, 
+          fitspw=fitspw, 
+          excludechans=False, 
+          solint='int',
+          fitorder=1,
+          want_cont=False) 
+
+plotms(vis = SB1_CO_mscontsub, xaxis = 'channel', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgtime = '1.e8')
+
+SB1_CO_mscontsubsplit = SB1_CO_mscontsub+'.split'
+#split out line channels for faster imaging
+os.system('rm -rf ' + SB1_CO_mscontsubsplit + '*')
+split2(vis=SB1_CO_mscontsub,
+       field = field,
+       spw='0:1870~2000',      
+       outputvis=SB1_CO_mscontsubsplit, 
+       datacolumn='data')
+
+plotms(vis = SB1_CO_mscontsubsplit, xaxis = 'channel', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgtime = '1.e8')
+
+SB1_CO_image = field+'_'+tag+'_CO21cube'
+os.system('rm -rf '+SB1_CO_image+'.*')
+clean(vis=SB1_CO_mscontsubsplit, 
+      imagename=SB1_CO_image,
+      mode = 'velocity',
+      psfmode = 'clark',  
+      imagermode='csclean',
+      weighting = 'briggs',
+      multiscale = [0, 10, 30, 50, 100],
+      robust = 1.0,
+      gain = 0.3, 
+      imsize = 500,
+      cell = '0.03arcsec',
+      start='-5.2km/s',
+      width='0.635km/s',
+      nchan=30, 
+      outframe='LSRK', 
+      veltype='radio', 
+      restfreq='230.53800GHz',
+      negcomponent=1, 
+      cyclefactor = 1, 
+      threshold = '10mJy',
+      interactive=True) 
 
 
 
