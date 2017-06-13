faea1490dc6e3b3aae3b2fcf0c85577c7a3a9ccb
Sean Andrews <sandrews@cfa.harvard.edu>
2017-06-13 16:10:35 -0400

    WSB 52 reduction script and results

---
+++ reduction_scripts/WSB_52.py
@@ -0,0 +1,257 @@
+"""
+This script is written for CASA 4.5.3
+Note that the imaging algorithms were rewritten significantly between 
+CASA 4.5.3 and CASA 4.7.2 
+"""
+
+field = 'WSB_52'
+
+##################################################################
+##################################################################
+## short baseline data
+##################################################################
+##################################################################
+
+SB1 = '/data/sandrews/LP/2016.1.00484.L/science_goal.uid___A001_Xbd4641_X1e/group.uid___A001_Xbd4641_X25/member.uid___A001_Xbd4641_X26/calibrated/calibrated_final.ms'
+SB1refant = 'DA46'
+tag = 'SB1'
+
+#split out all the data from the given field
+SB1_field = field+'_'+tag+'.ms'
+print(SB1_field) 
+os.system('rm -rf ' + SB1_field + '*')
+split2(vis=SB1,
+       field = field,    
+       outputvis=SB1_field,
+       datacolumn='data')
+
+"""
+This portion covers self-calibration and imaging of the continuum of the short baseline data
+"""
+# initial inspection of data before splitting out and averaging the continuum
+
+plotms(vis = SB1_field, xaxis = 'channel', yaxis = 'amplitude', field = field, 
+       ydatacolumn = 'data',avgtime = '1e8', avgscan = True, 
+       avgbaseline = True, iteraxis = 'spw')
+
+
+# spws 0, 4, 8 contain the CO 2-1 line
+contspws = '0~11'
+flagmanager(vis=SB1_field,mode='save', versionname='before_cont_flags')
+
+# Flag the CO 2-1 line
+flagchannels='0:1880~2250, 4:1880~2250, 8:1880~2250' 
+
+flagdata(vis=SB1_field,mode='manual', spw=flagchannels, flagbackup=False, field = field)
+
+# Average the channels within spws
+SB1_initcont = field+'_'+tag+'_initcont.ms'
+print(SB1_initcont)
+os.system('rm -rf ' + SB1_initcont + '*')
+split2(vis=SB1_field,
+       field = field,
+       spw=contspws,      
+       outputvis=SB1_initcont,
+       width=[480,8,8,8, 480, 8, 8, 8, 480, 8, 8, 8], 
+       datacolumn='data')
+
+
+# Restore flagged line channels
+flagmanager(vis=SB1_field,mode='restore',
+            versionname='before_cont_flags')
+
+# Check that amplitude vs. uvdist looks normal
+plotms(vis=SB1_initcont,xaxis='uvdist',yaxis='amp',coloraxis='spw', avgtime = '30')
+
+# Inspect individual antennae. We do this step here rather than before splitting because plotms will load the averaged continuum much faster 
+plotms(vis = SB1_initcont, xaxis = 'time', yaxis = 'phase', field = field, 
+       ydatacolumn = 'data',avgchannel = '16', observation = '0',
+       coloraxis = 'spw', iteraxis = 'antenna')
+
+plotms(vis = SB1_initcont, xaxis = 'time', yaxis = 'phase', field = field, 
+       ydatacolumn = 'data',avgchannel = '16', observation = '1',
+       coloraxis = 'spw', iteraxis = 'antenna')
+
+plotms(vis = SB1_initcont, xaxis = 'time', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgchannel = '16', observation = '0', 
+       coloraxis = 'spw', iteraxis = 'antenna') 
+
+plotms(vis = SB1_initcont, xaxis = 'time', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgchannel = '16', observation = '1', 
+       coloraxis = 'spw', iteraxis = 'antenna') 
+
+# check individual execution blocks
+SB1_EB1_initcontimage_dirty = field+'_'+tag+'_EB1_initcontinuum_dirty'
+os.system('rm -rf '+SB1_EB1_initcontimage_dirty+'.*')
+clean(vis=SB1_initcont, 
+      imagename=SB1_EB1_initcontimage_dirty, 
+      observation = '0', 
+      mode='mfs', 
+      psfmode='clark', 
+      imagermode='csclean', 
+      weighting='briggs', 
+      robust=0.5,
+      imsize=500,
+      cell='0.03arcsec', 
+      interactive=False, 
+      niter = 0)
+
+SB1_EB2_initcontimage_dirty = field+'_'+tag+'_EB2_initcontinuum_dirty'
+os.system('rm -rf '+SB1_EB2_initcontimage_dirty+'.*')
+clean(vis=SB1_initcont, 
+      imagename=SB1_EB2_initcontimage_dirty, 
+      observation = '1', 
+      mode='mfs', 
+      psfmode='clark', 
+      imagermode='csclean', 
+      weighting='briggs', 
+      robust=0.5,
+      imsize=500,
+      cell='0.03arcsec', 
+      interactive=False, 
+      niter = 0)
+
+SB1_EB3_initcontimage_dirty = field+'_'+tag+'_EB3_initcontinuum_dirty'
+os.system('rm -rf '+SB1_EB3_initcontimage_dirty+'.*')
+clean(vis=SB1_initcont,
+      imagename=SB1_EB3_initcontimage_dirty,
+      observation = '2',
+      mode='mfs',
+      psfmode='clark',
+      imagermode='csclean',
+      weighting='briggs',
+      robust=0.5,
+      imsize=500,
+      cell='0.03arcsec',
+      interactive=False,
+      niter = 0)
+# all look consistent
+
+# Initial clean
+SB1_initcontimage = field+'_'+tag+'_initcontinuum'
+os.system('rm -rf '+SB1_initcontimage+'.*')
+clean(vis=SB1_initcont, 
+      imagename=SB1_initcontimage, 
+      mode='mfs', 
+      psfmode='clark', 
+      imagermode='csclean', 
+      weighting='briggs', 
+      multiscale = [0, 10, 20, 30], 
+      robust=0.5,
+      gain = 0.3,
+      imsize=500,
+      cell='0.03arcsec', 
+      mask='circle[[254pix,236pix],1.0arcsec]',
+      interactive=True)
+
+# cleaned for 1 cycles (100 iterations)
+# peak: 36.9 mJy/beam
+# rms: 78 microJy/beam
+
+# First phase-self-cal
+SB1_p1 = field+'_'+tag+'.p1'
+os.system('rm -rf '+SB1_p1)
+gaincal(vis=SB1_initcont, caltable=SB1_p1, gaintype='T',  
+        spw=contspws, refant=SB1refant, calmode='p', 
+        solint='25s', minsnr=2.0, minblperant=4)
+
+plotcal(caltable=SB1_p1, xaxis = 'time', yaxis = 'phase',subplot=441,iteration='antenna', timerange = '2017/05/14/00:00:01~2017/05/14/11:59:59')
+
+plotcal(caltable=SB1_p1, xaxis = 'time', yaxis = 'phase',subplot=441,iteration='antenna', timerange = '2017/05/17/00:00:01~2017/05/17/11:59:59')
+
+plotcal(caltable=SB1_p1, xaxis = 'time', yaxis = 'phase',subplot=441,iteration='antenna', timerange = '2017/05/19/00:00:01~2017/05/19/11:59:59')
+
+
+applycal(vis=SB1_initcont, spw=contspws, gaintable=[SB1_p1], calwt=T, flagbackup=F)
+
+SB1_contms_p1 = field+'_'+tag+'_contp1.ms'
+os.system('rm -rf '+SB1_contms_p1)
+split2(vis=SB1_initcont, outputvis=SB1_contms_p1, datacolumn='corrected')
+
+
+SB1_contimage_p1 = field+'_'+tag+'_p1continuum'
+os.system('rm -rf '+SB1_contimage_p1+'.*')
+clean(vis=SB1_contms_p1, 
+      imagename=SB1_contimage_p1, 
+      mode='mfs', 
+      psfmode='clark', 
+      imagermode='csclean', 
+      weighting='briggs', 
+      multiscale = [0, 10, 20, 30], 
+      robust=0.5,
+      gain = 0.3,
+      imsize=500,
+      cell='0.03arcsec', 
+      mask='circle[[254pix,236pix],1.0arcsec]',
+      interactive=True)
+
+# cleaned for 2 cycles with 100 iterations each
+# peak: 40.0 mJy/beam
+# rms: 37.8 microJy/beam
+
+
+# Second round of phase cal didn't result in any noticeable improvement, so we move on to amplitude self-cal
+
+
+SB1_ap1 = field+'_'+tag+'.ap1'
+os.system('rm -rf '+SB1_ap1)
+gaincal(vis=SB1_contms_p1, caltable=SB1_ap1, gaintype='T',  
+        spw=contspws, refant=SB1refant, calmode='ap', 
+        solint='60s', minsnr=2.0, minblperant=4, solnorm = True)
+
+plotcal(caltable=SB1_ap1, xaxis = 'time', yaxis = 'amp',subplot=441,iteration='antenna', timerange = '2017/05/14/00:00:01~2017/05/14/11:59:59')
+
+plotcal(caltable=SB1_ap1, xaxis = 'time', yaxis = 'amp',subplot=441,iteration='antenna', timerange = '2017/05/17/00:00:01~2017/05/17/11:59:59')
+
+plotcal(caltable=SB1_ap1, xaxis = 'time', yaxis = 'amp',subplot=441,iteration='antenna', timerange = '2017/05/19/00:00:01~2017/05/19/11:59:59')
+
+
+applycal(vis=SB1_contms_p1, spw=contspws, gaintable=[SB1_ap1], calwt=T, flagbackup=F)
+
+SB1_contms_ap1 = field+'_'+tag+'_contap1.ms'
+os.system('rm -rf '+SB1_contms_ap1)
+split2(vis=SB1_contms_p1, outputvis=SB1_contms_ap1, datacolumn='corrected')
+
+SB1_contimage_ap1 = field+'_'+tag+'_ap1continuum'
+os.system('rm -rf '+SB1_contimage_ap1+'.*')
+clean(vis=SB1_contms_ap1, 
+      imagename=SB1_contimage_ap1, 
+      mode='mfs', 
+      psfmode='clark', 
+      imagermode='csclean', 
+      weighting='briggs', 
+      multiscale = [0, 10, 20, 30], 
+      robust=0.5,
+      gain = 0.3,
+      imsize=500,
+      cell='0.03arcsec', 
+      mask='circle[[254pix,236pix],1.0arcsec]',
+      interactive=True)
+
+# cleaned for 2 cycles of 100 iterations each
+# peak: 40.0 mJy/beam
+# rms: 36.9 microJy/beam
+
+### We are now done with self-cal of the continuum of SB1 and rename the final measurement set. 
+SB1_contms_final = field+'_'+tag+'_contfinal.ms'
+os.system('cp -r '+SB1_contms_ap1+' '+SB1_contms_final)
+
+
+# pushing the resolution
+SB1_contimage_ap1 = field+'_'+tag+'_ap1continuum_highres'
+os.system('rm -rf '+SB1_contimage_ap1+'.*')
+clean(vis=SB1_contms_ap1,
+      imagename=SB1_contimage_ap1,
+      mode='mfs',
+      psfmode='clark',
+      imagermode='csclean',
+      weighting='briggs',
+      multiscale = [0, 10, 20, 30], 
+      robust=-2.0,
+      gain = 0.3,
+      imsize=500,
+      cell='0.02arcsec',
+      mask='circle[[252pix,233pix],0.8arcsec]',
+      interactive=True)
+
+
