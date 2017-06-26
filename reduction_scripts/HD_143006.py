2070e070b46d09e7ed48d1d29d291691025968d6
j6626 <janehuang212@gmail.com>
2017-06-25 23:31:46 -0400

    updated HD 143006 script

--- reduction_scripts/HD_143006.py
+++ reduction_scripts/HD_143006.py
@@ -2,7 +2,10 @@
 This script is written for CASA 4.5.3
 Note that the imaging algorithms were rewritten significantly between CASA 4.5.3 and CASA 4.7.2 
 
-Datasets calibrated: 2016.1.00484.L/AS_205_b_06_TM1
+Datasets calibrated: 
+2016.1.00484.L/AS_205_b_06_TM1 (PI: Andrews)
+2015.1.00964.S/HD143006_a_06_TE (PI: Oberg)
+2015.1.00964.S/HD143006_a_06_TC (PI: Oberg)
 """
 
 
@@ -305,6 +308,77 @@ clean(vis=SB1_contms_ap1,
 SB1_contms_final = field+'_'+tag+'_contfinal.ms'
 os.system('cp -r '+SB1_contms_ap1+' '+SB1_contms_final)
 
+##############################
+# Reduction of CO data in SB1
+#############################
+
+#split out the CO 2-1 spectral window
+linespw = '0,4,8'
+SB1_CO_ms = field+'_'+tag+'_CO21.ms'
+os.system('rm -rf ' + SB1_CO_ms + '*')
+split2(vis=SB1,
+       field = field,
+       spw=linespw,      
+       outputvis=SB1_CO_ms, 
+       datacolumn='data')
+
+applycal(vis=SB1_CO_ms, spw='0~2',spwmap = [[0,0,0],[0,0,0],[0,0,0]], gaintable=[SB1_p1, SB1_p2, SB1_ap1], calwt=T, flagbackup=F)
+
+plotms(vis = SB1_CO_ms, xaxis = 'channel', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgtime = '1.e8',avgbaseline  =True)
+
+SB1_CO_mscontsub = SB1_CO_ms+'.contsub'
+os.system('rm -rf '+SB1_CO_mscontsub) 
+fitspw = '0:0~1860;2000~3839, 1:0~1860;2000~3839, 2:0~1860;2000~3839' # channels for fitting continuum
+uvcontsub(vis=SB1_CO_ms,
+          spw='0~2', 
+          fitspw=fitspw, 
+          excludechans=False, 
+          solint='int',
+          fitorder=1,
+          want_cont=False) 
+
+plotms(vis = SB1_CO_mscontsub, xaxis = 'channel', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgtime = '1.e8',avgbaseline  =True)
+
+SB1_CO_cvel = SB1_CO_mscontsub+'.cvel'
+
+os.system('rm -rf '+ SB1_CO_cvel)
+mstransform(vis = SB1_CO_mscontsub, outputvis = SB1_CO_cvel,  keepflags = False, datacolumn = 'data', regridms = True,mode='velocity',start='-2.0km/s',width='0.635km/s',nchan=30, outframe='LSRK', veltype='radio', restfreq='230.53800GHz')
+
+#check individual observation
+SB1_CO_image = field+'_'+tag+'_CO21cube'
+os.system('rm -rf '+SB1_CO_image+'.*')
+clean(vis=SB1_CO_cvel, 
+      imagename=SB1_CO_image,
+      mode = 'velocity',
+      psfmode = 'clark',  
+      imagermode='csclean',
+      weighting = 'briggs',
+      multiscale = [0, 10, 30, 50],
+      robust = 1.0,
+      gain = 0.3, 
+      imsize = 500,
+      cell = '0.03arcsec',
+      start='-2.0km/s',
+      width='0.635km/s',
+      nchan=30, 
+      outframe='LSRK', 
+      veltype='radio', 
+      restfreq='230.53800GHz',
+      negcomponent=1, 
+      cyclefactor = 1, 
+      threshold = '6mJy',
+      interactive=True) 
+
+os.system('rm -rf '+SB1_CO_image+'.mom0')
+immoments(axis = "spec",imagename=SB1_CO_image+'.image',moments=[0],outfile =SB1_CO_image+'.mom0', chans = '4~27')
+
+os.system('rm -rf '+SB1_CO_image+'.mom1')
+immoments(axis = "spec",imagename=SB1_CO_image+'.image',moments=[1],outfile =SB1_CO_image+'.mom1', chans = '4~27', includepix = [.01, 10])
+
+exportfits(imagename = SB1_CO_image+'.image', fitsimage = SB1_CO_image+'.image.fits')
+
 ##################################################################
 ##################################################################
 ## 2015.1.00964.S/HD143006_a_06_TE (as delivered to PI)
@@ -357,18 +431,15 @@ plotms(vis = SB2_initcont, xaxis = 'time', yaxis = 'phase', field = field,
        coloraxis = 'spw', iteraxis = 'antenna')
 
 
-plotms(vis = SB1_initcont, xaxis = 'time', yaxis = 'phase', field = field, 
+plotms(vis = SB2_initcont, xaxis = 'time', yaxis = 'phase', field = field, 
        ydatacolumn = 'data',avgchannel = '16', observation = '1',
        coloraxis = 'spw', iteraxis = 'antenna')
 
-flagdata(vis=SB1_initcont,mode='manual', spw='10', flagbackup=False, field = field, scan = '169, 177', antenna = 'DV15')
-
-
-plotms(vis = SB1_initcont, xaxis = 'time', yaxis = 'amp', field = field, 
+plotms(vis = SB2_initcont, xaxis = 'time', yaxis = 'amp', field = field, 
        ydatacolumn = 'data',avgchannel = '16', observation = '0', 
        coloraxis = 'spw', iteraxis = 'antenna') 
 
-plotms(vis = SB1_initcont, xaxis = 'time', yaxis = 'amp', field = field, 
+plotms(vis = SB2_initcont, xaxis = 'time', yaxis = 'amp', field = field, 
        ydatacolumn = 'data',avgchannel = '16', observation = '1', 
        coloraxis = 'spw', iteraxis = 'antenna') 
 
@@ -477,3 +548,328 @@ clean(vis=SB2_contms_ap1,
 # cleaned for 2 cycles of 100 iterations each
 # peak: 22.2 mJy/beam
 # rms: 65.6 microJy/beam
+
+##############################
+# Reduction of CO data in SB2
+#############################
+
+#split out the CO 2-1 spectral window
+linespw = '0,9'
+SB2_CO_ms = field+'_'+tag+'_CO21.ms'
+os.system('rm -rf ' + SB2_CO_ms + '*')
+split2(vis=SB2,
+       field = field,
+       spw=linespw,      
+       outputvis=SB2_CO_ms, 
+       datacolumn='data')
+
+applycal(vis=SB2_CO_ms, spw='0~1',spwmap = [[0,0],[0,0]], gaintable=[SB2_p1, SB2_ap1], calwt=T, flagbackup=F)
+
+plotms(vis = SB2_CO_ms, xaxis = 'channel', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgtime = '1.e8',avgbaseline  =True)
+
+SB2_CO_mscontsub = SB2_CO_ms+'.contsub'
+os.system('rm -rf '+SB2_CO_mscontsub) 
+fitspw = '0:200~450;550~959,1:200~450;550~959' # channels from 0 to 200 don't look great
+uvcontsub(vis=SB2_CO_ms,
+          spw='0~1', 
+          fitspw=fitspw, 
+          excludechans=False, 
+          solint='int',
+          fitorder=0,
+          want_cont=False) 
+
+plotms(vis = SB2_CO_mscontsub, xaxis = 'channel', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgtime = '1.e8',avgbaseline  =True)
+
+SB2_CO_cvel = SB2_CO_mscontsub+'.cvel'
+
+os.system('rm -rf '+ SB2_CO_cvel)
+mstransform(vis = SB2_CO_mscontsub, outputvis = SB2_CO_cvel,  keepflags = False, datacolumn = 'data', regridms = True,mode='velocity',start='-2.0km/s',width='0.635km/s',nchan=30, outframe='LSRK', veltype='radio', restfreq='230.53800GHz')
+
+#check individual observation
+SB2_CO_image = field+'_'+tag+'_CO21cube'
+os.system('rm -rf '+SB2_CO_image+'.*')
+clean(vis=SB2_CO_cvel, 
+      imagename=SB2_CO_image,
+      mode = 'velocity',
+      psfmode = 'clark',  
+      imagermode='csclean',
+      weighting = 'briggs',
+      multiscale = [0, 10, 30, 50],
+      robust = 1.0,
+      gain = 0.3, 
+      imsize = 500,
+      cell = '0.05arcsec',
+      start='-2.0km/s',
+      width='0.635km/s',
+      nchan=30, 
+      outframe='LSRK', 
+      veltype='radio', 
+      restfreq='230.53800GHz',
+      negcomponent=1, 
+      cyclefactor = 1, 
+      threshold = '6mJy',
+      interactive=True) 
+
+os.system('rm -rf '+SB2_CO_image+'.mom0')
+immoments(axis = "spec",imagename=SB2_CO_image+'.image',moments=[0],outfile =SB2_CO_image+'.mom0', chans = '4~27')
+
+os.system('rm -rf '+SB2_CO_image+'.mom1')
+immoments(axis = "spec",imagename=SB2_CO_image+'.image',moments=[1],outfile =SB2_CO_image+'.mom1', chans = '4~27', includepix = [.025, 10])
+
+##################################################################
+##################################################################
+## 2015.1.00964.S/HD143006_a_06_TC (downloaded from archive)
+##################################################################
+##################################################################
+
+SB3 = '/pool/firebolt1/HD143006_TC.ms' #replace as appropriate
+SB3refant = 'DV16'
+tag = 'SB3'
+field = 'HD143006'
+
+# initial inspection of data before splitting out and averaging the continuum
+
+plotms(vis = SB3, xaxis = 'channel', yaxis = 'amplitude', field = field, 
+       ydatacolumn = 'data',avgtime = '1e8', avgscan = True, 
+       avgbaseline = True, iteraxis = 'spw')
+
+contspws = '0,1,2' #all the upper sideband windows
+
+flagmanager(vis=SB3,mode='save', versionname='before_cont_flags')
+
+# Flag the CO 2-1 line
+flagchannels='0:100~200, 0:450~550' #modify as appropriate for the given field
+
+flagdata(vis=SB3,mode='manual', spw=flagchannels, flagbackup=False, field = field)
+
+# Average the channels within spws
+SB3_initcont = field+'_'+tag+'_initcont.ms'
+print SB3_initcont #just to double check for yourself that the name is actually ok
+os.system('rm -rf ' + SB3_initcont + '*')
+split2(vis=SB3,
+       field = field,
+       spw=contspws,      
+       outputvis=SB3_initcont,
+       width=[960,960,256], # ALMA recommends channel widths <= 125 MHz in Band 6 to avoid bandwidth smearing
+       datacolumn='data')
+
+# Restore flagged line channels
+flagmanager(vis=SB3,mode='restore',
+            versionname='before_cont_flags')
+
+# Check that amplitude vs. uvdist looks normal
+plotms(vis=SB3_initcont,xaxis='uvdist',yaxis='amp',coloraxis='spw', avgtime = '30s',avgscan = True)
+
+# Inspect individual antennae. We do this step here rather than before splitting because plotms will load the averaged continuum much faster 
+
+plotms(vis = SB3_initcont, xaxis = 'time', yaxis = 'phase', field = field, 
+       ydatacolumn = 'data',avgchannel = '15', observation = '0',
+       coloraxis = 'spw', iteraxis = 'antenna')
+
+
+plotms(vis = SB3_initcont, xaxis = 'time', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgchannel = '16', observation = '0', 
+       coloraxis = 'spw', iteraxis = 'antenna') 
+
+#start of self-calibration with model from higher S/N HD 143006 observations
+
+ft(vis = SB3_initcont, model = 'HD_143006_SB1_ap1continuum.model') 
+
+# First phase-self-cal
+SB3_p1 = field+'_'+tag+'.p1'
+os.system('rm -rf '+SB3_p1)
+gaincal(vis=SB3_initcont, caltable=SB3_p1, gaintype='T', combine = 'spw', 
+        spw='0~2', refant=SB3refant, calmode='p', 
+        solint='45s', minsnr=2.0, minblperant=4)
+
+plotcal(caltable=SB3_p1, xaxis = 'time', yaxis = 'phase',subplot=441,iteration='antenna')
+
+applycal(vis=SB3_initcont, spw='0~2', spwmap = 3*[0], gaintable=[SB3_p1], calwt=T, flagbackup=F)
+
+SB3_contms_p1 = field+'_'+tag+'_contp1.ms'
+os.system('rm -rf '+SB3_contms_p1)
+split2(vis=SB3_initcont, outputvis=SB3_contms_p1, datacolumn='corrected')
+
+
+SB3_contimage_p1 = field+'_'+tag+'_p1continuum'
+os.system('rm -rf '+SB3_contimage_p1+'.*')
+clean(vis=SB3_contms_p1, 
+      imagename=SB3_contimage_p1, 
+      mode='mfs', 
+      psfmode='clark', 
+      imagermode='csclean', 
+      weighting='briggs', 
+      multiscale = [0, 10, 20, 30], # this is really up to the user. The choices here matter less than they do for the extended data. 
+      robust=0.5,
+      gain = 0.3,
+      imsize=500,
+      cell='0.05arcsec', 
+      mask='circle[[250pix,250pix],1.1arcsec]',
+      interactive=True)
+
+# cleaned for 2 cycles of 100 iterations each
+# peak: 21.6 mJy/beam
+# rms: 76.9 microJy/beam
+
+
+SB3_ap1 = field+'_'+tag+'.ap1'
+os.system('rm -rf '+SB3_ap1)
+gaincal(vis=SB3_contms_p1, caltable=SB3_ap1, gaintype='T', combine = 'spw', 
+        spw='0~2', refant=SB3refant, calmode='ap', 
+        solint='inf', minsnr=2.0, minblperant=4, solnorm = True)
+
+plotcal(caltable=SB3_ap1, xaxis = 'time', yaxis = 'amp',subplot=441,iteration='antenna')
+
+applycal(vis=SB3_contms_p1, spw='0~2', spwmap = 3*[0], gaintable=[SB3_ap1], calwt=T, flagbackup=F)
+
+SB3_contms_ap1 = field+'_'+tag+'_contap1.ms'
+os.system('rm -rf '+SB3_contms_ap1)
+split2(vis=SB3_contms_p1, outputvis=SB3_contms_ap1, datacolumn='corrected')
+
+SB3_contimage_ap1 = field+'_'+tag+'_ap1continuum'
+os.system('rm -rf '+SB3_contimage_ap1+'.*')
+clean(vis=SB3_contms_ap1, 
+      imagename=SB3_contimage_ap1, 
+      mode='mfs', 
+      psfmode='clark', 
+      imagermode='csclean', 
+      weighting='briggs', 
+      multiscale = [0, 10, 20, 30], # this is really up to the user. The choices here matter less than they do for the extended data. 
+      robust=0.5,
+      gain = 0.3,
+      imsize=500,
+      cell='0.05arcsec', 
+      mask='circle[[250pix,250pix],1.1arcsec]',
+      interactive=True)
+
+# cleaned for 2 cycles of 100 iterations each
+# peak: 21.6 mJy/beam
+# rms: 72.5 microJy/beam
+
+##############################
+# Reduction of CO data in SB3
+#############################
+
+#split out the CO 2-1 spectral window
+linespw = '0'
+SB3_CO_ms = field+'_'+tag+'_CO21.ms'
+os.system('rm -rf ' + SB3_CO_ms + '*')
+split2(vis=SB3,
+       field = field,
+       spw=linespw,      
+       outputvis=SB3_CO_ms, 
+       datacolumn='data')
+
+applycal(vis=SB3_CO_ms, spw='0', gaintable=[SB3_p1, SB3_ap1], calwt=T, flagbackup=F)
+
+plotms(vis = SB3_CO_ms, xaxis = 'channel', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgtime = '1.e8',avgbaseline  =True)
+
+SB3_CO_mscontsub = SB3_CO_ms+'.contsub'
+os.system('rm -rf '+SB3_CO_mscontsub) 
+fitspw = '0:200~450;550~959' # channels from 0 to 200 don't look great
+uvcontsub(vis=SB3_CO_ms,
+          spw='0', 
+          fitspw=fitspw, 
+          excludechans=False, 
+          solint='int',
+          fitorder=0,
+          want_cont=False) 
+
+plotms(vis = SB3_CO_mscontsub, xaxis = 'channel', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgtime = '1.e8',avgbaseline  =True)
+
+SB3_CO_cvel = SB3_CO_mscontsub+'.cvel'
+
+os.system('rm -rf '+ SB3_CO_cvel)
+mstransform(vis = SB3_CO_mscontsub, outputvis = SB3_CO_cvel,  keepflags = False, datacolumn = 'data', regridms = True,mode='velocity',start='-2.0km/s',width='0.635km/s',nchan=30, outframe='LSRK', veltype='radio', restfreq='230.53800GHz')
+
+#check individual observation
+SB3_CO_image = field+'_'+tag+'_CO21cube'
+os.system('rm -rf '+SB3_CO_image+'.*')
+clean(vis=SB3_CO_cvel, 
+      imagename=SB3_CO_image,
+      mode = 'velocity',
+      psfmode = 'clark',  
+      imagermode='csclean',
+      weighting = 'briggs',
+      multiscale = [0, 10, 30, 50],
+      robust = 1.0,
+      gain = 0.3, 
+      imsize = 500,
+      cell = '0.05arcsec',
+      start='-2.0km/s',
+      width='0.635km/s',
+      nchan=30, 
+      outframe='LSRK', 
+      veltype='radio', 
+      restfreq='230.53800GHz',
+      negcomponent=1, 
+      cyclefactor = 1, 
+      threshold = '6mJy',
+      interactive=True) 
+
+
+
+########## Data combinations
+
+# concatenate all short baseline continuum observations
+
+concat(vis = [SB1_contms_ap1,SB2_contms_ap1,SB3_contms_ap1], concatvis = 'HD_143006_allSB_cont.ms', freqtol = '20MHz', dirtol = '0.1arcsec', respectname = False, copypointing = False)
+
+
+
+clean(vis='HD_143006_allSB_cont.ms', 
+      imagename='HD143006_combinedcont', 
+      mode='mfs', 
+      psfmode='clark', 
+      imagermode='csclean', 
+      weighting='briggs', 
+      multiscale = [0, 10, 20, 30], # this is really up to the user. The choices here matter less than they do for the extended data. 
+      robust=-2,
+      gain = 0.05,
+      imsize=500,
+      cell='0.03arcsec', 
+      mask='circle[[250pix,250pix],1.0arcsec]',
+      interactive=True)
+
+#currently have a helpdesk ticket pending because of problems with MS incompatibility....
+
+
+clean(vis=[SB1_CO_cvel,SB2_CO_cvel,SB3_CO_cvel], 
+      imagename='HD143006_CO_combined',
+      mode = 'velocity',
+      psfmode = 'clark',  
+      imagermode='csclean',
+      weighting = 'briggs',
+      multiscale = [0, 10, 20, 30],
+      robust = 0.5,
+      gain = 0.1, 
+      imsize = 500,
+      cell = '0.03arcsec',
+      start='-2.0km/s',
+      width='0.635km/s',
+      nchan=30, 
+      outframe='LSRK', 
+      veltype='radio', 
+      restfreq='230.53800GHz',
+      negcomponent=1, 
+      cyclefactor = 1, 
+      threshold = '6mJy',
+      interactive=True) 
+
+immoments(axis = "spec",imagename='HD143006_CO_combined.image',moments=[0],outfile ='HD143006_CO_combined.mom0', chans = '4~27')
+
+immoments(axis = "spec",imagename='HD143006_CO_combined.image',moments=[1],outfile ='HD143006_CO_combined.mom1', chans = '4~27', includepix = [.02, 10])
+
+#immoments(axis = "spec",imagename='HD143006_CO_spectralhighres.image',moments=[0],outfile ='HD143006_CO_spectralhighres.mom0', chans = '11~42')
+
+#immoments(axis = "spec",imagename='HD143006_CO_spectralhighres.image',moments=[1],outfile ='HD143006_CO_spectralhighres.mom1', chans = '11~42', includepix = [.02, 10])
+
+
+
+
+ 
+
