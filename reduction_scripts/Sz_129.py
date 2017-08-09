4762e85f329a3dc5e1bd1ace2334a8801f3f8dcb
j6626 <janehuang212@gmail.com>
2017-08-08 23:07:03 -0400

    updated Sz 129 script

--- reduction_scripts/Sz_129.py
+++ reduction_scripts/Sz_129.py
@@ -1,6 +1,9 @@
 """
 This script is written for CASA 4.5.3
 Note that the imaging algorithms were rewritten significantly between CASA 4.5.3 and CASA 4.7.2 
+
+Datasets calibrated: 
+2016.1.00484.L/GW_Lup_a_06_TM1  (PI: Andrews)
 """
 
 field = 'Sz_129'
@@ -216,5 +219,72 @@ clean(vis=SB1_contms_ap1,
 ### We are now done with self-cal of the continuum of SB1 and rename the final measurement set. 
 SB1_contms_final = field+'_'+tag+'_contfinal.ms'
 os.system('cp -r '+SB1_contms_ap1+' '+SB1_contms_final)
+
+##############################
+# Reduction of CO data in SB1
+#############################
+SB1_field = '/data/sandrews/LP/2016.1.00484.L/science_goal.uid___A001_Xbd4641_X1e/group.uid___A001_Xbd4641_X1f/member.uid___A001_Xbd4641_X20/calibrated/Sz_129_SB1.ms'
+
+applycal(SB1_field, gaintable=[SB1_p1, SB1_ap1], calwt=T, flagbackup=F)
+
+#split out the CO 2-1 spectral window
+linespw = '0, 4'
+SB1_CO_ms = field+'_'+tag+'_CO21.ms'
+os.system('rm -rf ' + SB1_CO_ms + '*')
+split2(vis=SB1_field,
+       field = field,
+       spw=linespw,      
+       outputvis=SB1_CO_ms, 
+       datacolumn='corrected')
+
+plotms(vis = SB1_CO_ms, xaxis = 'channel', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgtime = '1.e8',avgbaseline  =True)
+
+
+
+SB1_CO_mscontsub = SB1_CO_ms+'.contsub'
+os.system('rm -rf '+SB1_CO_mscontsub) 
+fitspw = '0:0~1880;1960~3839, 1:0~1880;1960~3839' # channels for fitting continuum
+uvcontsub(vis=SB1_CO_ms,
+          spw='0~1', 
+          fitspw=fitspw, 
+          excludechans=False, 
+          solint='int',
+          fitorder=1,
+          want_cont=False) 
+
+plotms(vis = SB1_CO_mscontsub, xaxis = 'channel', yaxis = 'amp', field = field, 
+       ydatacolumn = 'data',avgtime = '1.e8',avgbaseline  =True)
+
+
+CO_cvel = SB1_CO_mscontsub+'.cvel'
+
+os.system('rm -rf '+ CO_cvel)
+mstransform(vis = SB1_CO_mscontsub, outputvis = CO_cvel,  keepflags = False,datacolumn = 'data', regridms = True,mode='velocity',start='-5km/s',width='0.35km/s',nchan=60, outframe='LSRK', veltype='radio', restfreq='230.53800GHz')
+
+SB1_CO_image = field+'_'+tag+'_CO21cube'
+os.system('rm -rf '+SB1_CO_image+'.*')
+clean(vis=CO_cvel, 
+      imagename=SB1_CO_image,
+      mode = 'velocity',
+      psfmode = 'clark',  
+      imagermode='csclean',
+      weighting = 'briggs',
+      multiscale = [0, 10, 30, 50],
+      robust = 1.0,
+      gain = 0.3, 
+      imsize = 500,
+      cell = '0.03arcsec',
+      start='-5km/s',
+      width='0.35km/s',
+      nchan=60, 
+      outframe='LSRK', 
+      veltype='radio', 
+      restfreq='230.53800GHz',
+      negcomponent=1, 
+      cyclefactor = 1, 
+      threshold = '10mJy',
+      interactive=True) 
+
 
 
