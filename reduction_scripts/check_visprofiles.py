b0cff52741ac4a894e8b33bb5e7518f19982eb28
Sean Andrews <sandrews@cfa.harvard.edu>
2018-02-20 15:01:04 -0500

    cleaned up check_visprofiles.py for ease of use

--- reduction_scripts/check_visprofiles.py
+++ reduction_scripts/check_visprofiles.py
@@ -1,88 +1,56 @@
 import numpy as np
-import os
-import sys
 import matplotlib.pyplot as plt
-sys.path.append('/home/sandrews/mypy/')
 from deproject_vis import deproject_vis
 
-# definitions
-incl = 36.2
-PA = 41.8
-offx = -0.2843
-offy = -0.1646
+# files to compare
+files = ['SB_quick', 'LB0_quick', 'LB1_quick']
 
-incl = 39.8
-PA = 36.4
-offx = -0.2919
-offy = -0.1735
+# manual re-scalings to check flux calibration
+rescl = [1.0, 1.0, 1.0]
 
+# geometric parameters 
+incl = 30.
+PA   = 52.
+offx = 0.118
+offy = -0.386
 
-# read in the data
-adat = 'SB_quick.vis.npz'
-inpf = np.load(adat)
-u    = inpf['u']
-v    = inpf['v']
-vis = inpf['Vis']
-wgt = inpf['Wgt']
-vpa  = deproject_vis([u, v, vis, wgt], bins=10.+10.*np.arange(1500),
-                     incl=incl, PA=PA, offx=offx, offy=offy)
-vpa_rho, vpa_vis, vpa_sig = vpa
+# bin centers (use only linear scale for now)
+uvbins = 10.+10.*np.arange(100)
 
-bdat = 'LB0_quick.vis.npz'
-inpf = np.load(bdat)
-u    = inpf['u']
-v    = inpf['v']
-dvis = inpf['Vis']
-dwgt = inpf['Wgt']
-uv   = u, v
-vpb  = deproject_vis([u, v, dvis, dwgt], bins=10.+10.*np.arange(1500),
-                     incl=incl, PA=PA, offx=offx, offy=offy)
-vpb_rho, vpb_vis, vpb_sig = vpb
+# colors
+cols = ['r', 'g', 'b']
 
-cdat = 'LB1_quick_rescaled.vis.npz'
-inpf = np.load(cdat)
-u    = inpf['u']
-v    = inpf['v']
-dvis = inpf['Vis']
-dwgt = inpf['Wgt']
-uv   = u, v
-vpc  = deproject_vis([u, v, dvis, dwgt], bins=10.+10.*np.arange(1500),
-                     incl=incl, PA=PA, offx=offx, offy=offy)
-vpc_rho, vpc_vis, vpc_sig = vpc
+# loop through files and plot the deprojected profiles
+minvis = np.zeros((len(files)))
+maxvis = np.zeros((len(files)))
+for i in range(len(files)):
 
-rdat = 'combined_quick.vis.npz'
-inpf = np.load(rdat)
-u    = inpf['u']
-v    = inpf['v']
-dvis = inpf['Vis']
-dwgt = inpf['Wgt']
-uv   = u, v
-vpr  = deproject_vis([u, v, dvis, dwgt], bins=10.+10.*np.arange(1500),
-                     incl=incl, PA=PA, offx=offx, offy=offy)
-vpr_rho, vpr_vis, vpr_sig = vpr
+    # read in the data
+    inpf = np.load(files[i]+'.vis.npz')
+    u    = inpf['u']
+    v    = inpf['v']
+    vis  = rescl[i]*inpf['Vis']
+    wgt  = inpf['Wgt']
 
+    # deproject the visibilities and do the annular averaging
+    vp   = deproject_vis([u, v, vis, wgt], bins=uvbins, incl=incl, PA=PA, 
+                         offx=offx, offy=offy)
+    vp_rho, vp_vis, vp_sig = vp
 
+    # calculate min, max of deprojected, averaged reals (for visualization)
+    minvis[i] = np.min(vp_vis.real)
+    maxvis[i] = np.max(vp_vis.real)
 
+    # plot the profile
+    plt.plot(1e-3*vp_rho, vp_vis.real, 'o'+cols[i], markersize=2.8)
 
-plt.axis([00, 10000, -0.01, 0.095])
-plt.plot([0., 10000.], [0., 0.], '--k')
-#plt.errorbar(1e-3*vpa_rho, vpa_vis.real, yerr=vpa_sig.real, 
-#             fmt='or', markersize=2.8)
-#plt.errorbar(1e-3*vpb_rho, 1.0*vpb_vis.real, yerr=vpb_sig.real, 
-#             fmt='og', markersize=2.8)
-#plt.errorbar(1e-3*vpc_rho, 1.0*vpc_vis.real, yerr=vpc_sig.real, 
-#             fmt='ob', markersize=2.8)
-plt.errorbar(1e-3*vpr_rho, 1.0*vpr_vis.real, yerr=vpr_sig.real,
-             fmt='ok', markersize=2.8)
-plt.errorbar(1e-3*vpr_rho, 1.0*vpr_vis.imag, yerr=vpr_sig.imag,
-             fmt='or', markersize=2.8, alpha=0.5)
-
-#plt.errorbar(1e-3*vpa_rho, vpa_vis.imag, yerr=vpa_sig.imag, 
-#             fmt='or', markersize=2.8)
-#plt.errorbar(1e-3*vpb_rho, 1.05*vpb_vis.imag, yerr=vpb_sig.imag, 
-#             fmt='og', markersize=2.8)
-#plt.errorbar(1e-3*vpc_rho, 1.35*vpc_vis.imag, yerr=vpc_sig.imag, 
-#             fmt='ob', markersize=2.8)
-
+# plot adjustments
+allmaxvis = np.max(maxvis)
+allminvis = np.min(minvis)
+if ((allminvis < 0) or (allminvis-0.1*allmaxvis < 0)):
+    plt.axis([0, 1000, allminvis-0.1*allmaxvis, 1.1*allmaxvis])
+else: plt.axis([0, 1000, 0., 1.1*allmaxvis])
+plt.plot([0, 1000], [0, 0], '--k')
+plt.xlabel('deprojected baseline length [klambda]')
+plt.ylabel('average real [Jy]')
 plt.show()
-
