9e720d4d37fd4a2ed2e0c2aecd1d8f004d4d4fc2
Sean Andrews <sandrews@cfa.harvard.edu>
2018-02-20 14:08:01 -0500

    made ExportMS.py a call-able function

--- reduction_scripts/ExportMS.py
+++ reduction_scripts/ExportMS.py
@@ -1,64 +1,70 @@
 """
-This script should (temporarily at least) be preceded by a crude workaround 
-for passing the MS filename (until I make a formal CASA task):
+This function should produce a numpy save file of the spectrally averaged (and 
+polarization-averaged) input MS.  The 'filename' parameter should end in '.ms'.
 
-os.system('echo '+MS_filename+' > filename.txt')
+Just place:
+
 execfile('ExportMS.py')
+
+at the top of your CASA script and run with
+
+ExportMS(filename)
 
 """
 import numpy as np
 
-# load the filename, strip off the '.ms'
-f = open('filename.txt', 'r')
-MS_filename = f.read().replace('.ms', '').replace('\n', '')
+def ExportMS(filename):
 
-# get information about spectral windows
-ms.open(MS_filename+'.ms')
-spwInfo = ms.getspectralwindowinfo()
-ms.close()
-nspw = len(spwInfo)
-chanlist = [(spwInfo[str(i)])['NumChan'] for i in range(nspw)]
+    # strip off the '.ms'
+    MS_filename = filename.replace('.ms', '')
 
-# spectral averaging (1 channel per SPW)
-os.system('rm -rf '+MS_filename+'_spavg.ms*')
-split(vis=MS_filename+'.ms', width=chanlist, datacolumn='data',
-      outputvis=MS_filename+'_spavg.ms')
+    # get information about spectral windows
+    ms.open(MS_filename+'.ms')
+    spwInfo = ms.getspectralwindowinfo()
+    ms.close()
+    nspw = len(spwInfo)
+    chanlist = [(spwInfo[str(i)])['NumChan'] for i in range(nspw)]
 
-# get the data tables
-tb.open(MS_filename+'_spavg.ms')
-data   = np.squeeze(tb.getcol("DATA"))
-flag   = np.squeeze(tb.getcol("FLAG"))
-uvw    = tb.getcol("UVW")
-weight = tb.getcol("WEIGHT")
-spwid  = tb.getcol("DATA_DESC_ID")
-tb.close()
+    # spectral averaging (1 channel per SPW)
+    os.system('rm -rf '+MS_filename+'_spavg.ms*')
+    split(vis=MS_filename+'.ms', width=chanlist, datacolumn='data',
+          outputvis=MS_filename+'_spavg.ms')
 
-# get frequency information
-tb.open(MS_filename+'_spavg.ms/SPECTRAL_WINDOW')
-freq = tb.getcol("CHAN_FREQ")
-tb.close()
+    # get the data tables
+    tb.open(MS_filename+'_spavg.ms')
+    data   = np.squeeze(tb.getcol("DATA"))
+    flag   = np.squeeze(tb.getcol("FLAG"))
+    uvw    = tb.getcol("UVW")
+    weight = tb.getcol("WEIGHT")
+    spwid  = tb.getcol("DATA_DESC_ID")
+    tb.close()
 
-# get rid of any flagged columns
-crap   = np.any(flag, axis=0)
-good   = np.squeeze([crap == False])
-data   = data[:,good]
-weight = weight[:,good]
-uvw    = uvw[:,good]
-spwid  = spwid[good]
+    # get frequency information
+    tb.open(MS_filename+'_spavg.ms/SPECTRAL_WINDOW')
+    freq = tb.getcol("CHAN_FREQ")
+    tb.close()
 
-# break out the spatial frequency coordinates in **lambda** units
-freqlist = freq[0]
-findices = lambda ispw: freqlist[ispw]
-freqs = findices(spwid)
-u = uvw[0,:] * freqs / 2.9979e8
-v = uvw[1,:] * freqs / 2.9979e8
+    # get rid of any flagged columns
+    crap   = np.any(flag, axis=0)
+    good   = np.squeeze([crap == False])
+    data   = data[:,good]
+    weight = weight[:,good]
+    uvw    = uvw[:,good]
+    spwid  = spwid[good]
 
-# average the polarizations
-Re  = np.sum(data.real*weight, axis=0) / np.sum(weight, axis=0)
-Im  = np.sum(data.imag*weight, axis=0) / np.sum(weight, axis=0)
-Vis = Re + 1j*Im
-Wgt = np.sum(weight, axis=0)
+    # break out the spatial frequency coordinates in **lambda** units
+    freqlist = freq[0]
+    findices = lambda ispw: freqlist[ispw]
+    freqs = findices(spwid)
+    u = uvw[0,:] * freqs / 2.9979e8
+    v = uvw[1,:] * freqs / 2.9979e8
 
-# output to a numpy save file
-os.system('rm -rf '+MS_filename+'.vis.npz')
-np.savez(MS_filename+'.vis', u=u, v=v, Vis=Vis, Wgt=Wgt)
+    # average the polarizations
+    Re  = np.sum(data.real*weight, axis=0) / np.sum(weight, axis=0)
+    Im  = np.sum(data.imag*weight, axis=0) / np.sum(weight, axis=0)
+    Vis = Re + 1j*Im
+    Wgt = np.sum(weight, axis=0)
+
+    # output to a numpy save file
+    os.system('rm -rf '+MS_filename+'.vis.npz')
+    np.savez(MS_filename+'.vis', u=u, v=v, Vis=Vis, Wgt=Wgt)
