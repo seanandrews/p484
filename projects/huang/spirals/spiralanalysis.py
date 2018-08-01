bc5969e4e3535de8d1c9bfe3253a0666657ea7e0
j6626 <janehuang212@gmail.com>
2018-08-01 00:44:02 -0400

    updated spirals scripts

--- projects/huang/spirals/spiralanalysis.py
+++ projects/huang/spirals/spiralanalysis.py
@@ -9,6 +9,10 @@ import emcee, corner
 def logspiral(theta, r0, b):
     return r0*np.exp(b*theta)
 
+
+def archspiral(theta, a, c):
+    return a+c*theta
+
 def transform_spiral(thetavals, rvals, distance, PA, incl):
     
     xpvals = rvals*np.cos(thetavals)*np.cos(incl*np.pi/180.)/distance
@@ -24,10 +28,12 @@ def estimate_errors(cont, xvals, yvals, rvals):
     multiplier = rvals/np.sqrt(xvals**2+yvals**2)*1/cont.src_distance
     return sigma*multiplier
 
-def lnlike(spiral_params, thetavals, rvals,sig):
-    r0, b = spiral_params
+def lnlike(spiral_params, thetavals, rvals,sig, spiraltype = 'log'):
     inv_sigma2 = 1.0/sig**2
-    modelvals = r0*np.exp(b*thetavals)
+    if spiraltype=='log':
+        modelvals = logspiral(thetavals, spiral_params[0],spiral_params[1])
+    elif spiraltype=='archimedean':
+        modelvals = archspiral(thetavals, spiral_params[0],spiral_params[1])
     return -0.5*(np.sum(inv_sigma2*(rvals-modelvals)**2 - np.log(inv_sigma2)))
 
   
@@ -39,10 +45,13 @@ def run_mcmc(nwalkers, nthreads, nsteps, initial_pos, lnprob, rvals,thetavals, e
     sampler.run_mcmc(starting_pos, nsteps)
     return sampler
     
-def plot_corner(sampler, burnin, skip):
+def plot_corner(sampler, burnin, skip, spiraltype = 'log'):
     ndim = sampler.chain.shape[2]
     samples = sampler.chain[:, burnin::skip, :].reshape((-1, ndim))
-    fig = corner.corner(samples,  labels=["r0", "b"], quantiles = [0.16, 0.5, 0.84], show_titles = True, title_fmt = ".4f")
+    if spiraltype=='log':
+        fig = corner.corner(samples,  labels=["r0", "b"], quantiles = [0.16, 0.5, 0.84], show_titles = True, title_fmt = ".4f")
+    elif spiraltype=='archimedean':
+        fig = corner.corner(samples,  labels=["a", "c"], quantiles = [0.16, 0.5, 0.84], show_titles = True, title_fmt = ".4f")
     plt.show(block = False)
     return samples
 
